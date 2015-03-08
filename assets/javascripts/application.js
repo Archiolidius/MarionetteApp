b2f36fe82d1359f8fef7e40eb18b95d0aa93ec63
Archiolidius <Archiolidius@gmail.com>
2015-03-08 16:33:19 +0200

    commit number 2

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,19 +1,5 @@
 Application = new Backbone.Marionette.Application();
 
-Backbone.Marionette.TemplateCache.prototype.loadTemplate = function(templateId) {
-
-    var template = templateId;
-
-    if (!template || template.length === 0){
-        var msg = "Could not find template: '" + templateId + "'";
-        var err = new Error(msg);
-        err.name = "NoTemplateError";
-        throw err;
-    }
-
-    return template;
-};
-
 Application.addRegions({
     mainRegion: '#content'
 });
